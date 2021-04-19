e312ee3a47485667ec15d273bab2058eacc64e1b
Jonathan Smith <porsmitj2@gmail.com>
2021-04-19 18:42:17 +0100

    formatted

--- src/BareMinimum.cpp
+++ src/BareMinimum.cpp
@@ -1,15 +1,15 @@
 // =============================================================================
-// The bare minimum of code needed to start an Arduino sketch. 
+// The bare minimum of code needed to start an Arduino sketch.
 // =============================================================================
 
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-
 }
